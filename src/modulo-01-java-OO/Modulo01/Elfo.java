4c9852f770aa755cfb231d157b641d03adfe87f7
Angelo Viana <angelojunior50@yahoo.com.br>
2015-09-16 13:37:44 -0300

    Consertando o gerador e retirando retundancias

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,26 +1,35 @@
+import java.util.Random;
+
 public class Elfo {
     private String nome;
     private int flechas;
-    private experiencia = 0;
+    private int experiencia = 0;
     
     public Elfo(String n) {
         nome = n;
         flechas = 42;
     }
-	
+    
     public Elfo(String n, int flech){
         nome = n;
         flechas = flech;
     }
-	
-    public atirarFlechas(){
-		experiencia++;
-		flechas--;
+    
+    public boolean acertar(){
+        Random gerador = new Random();
+        boolean acertou = gerador.nextBoolean();
+        return acertou;
     }
-	
-	public atirarFlechaRefactory(this.flechas, this.experiencia){
-		flechas--;
-		if(boolean acertar == true)
+    
+    public void atirarFlechas(){
+        flechas--;
+        if(acertar())
 			experiencia++;
-	}
+    }
+    
+    public void atirarFlechaRefactory(){
+        flechas--;
+        if(acertar())
+            experiencia++;
+    }
 }
