4affe271613e48bb759048055ddd875b3c8db056
Angelo Viana <angelojunior50@yahoo.com.br>
2015-09-17 17:32:54 -0300

    exercicio 3

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -1,10 +1,25 @@
 public class Dwarf{
     private int vida = 110;
-    
+    private Status status = Status.VIVO;
+    private String nome;
+    private int experiencia;
+
+    public Dwarf (String nome){
+        this.nome = nome;
+    }
+
     public void receberFlechada(){
         this.vida -= 10;
+        if(vida<=0){
+            status = Status.MORTO;
+        }
     }
+
     public int getVida(){
         return vida;
+    }
+
+    public Status getStatus(){
+        return this.status; 
     }
 }
