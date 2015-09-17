3d1a98fb5aa8cd6e1bd9faa268c519ca6938efbd
Angelo <angelojunior50@yahoo.com.br>
2015-09-17 20:11:48 -0300

    adicionando ehBissexto e atributos

--- src/modulo-01-java-OO/Modulo01/DataTerceiraEra.java
+++ src/modulo-01-java-OO/Modulo01/DataTerceiraEra.java
@@ -2,7 +2,21 @@ public class DataTerceiraEra{
     private int dia;
     private int mes;
     private int ano;
-
+    
+    public DataTerceiraEra(int dia, int mes, int ano){
+    this.dia = dia;
+    this.mes = mes;
+    this.ano = ano;
+    }
+    
+    public boolean ehBissexto(){
+    if((this.ano % 4 == 0) && (this.ano % 100 != 0)){
+        return true;
+    }else{
+        return false;
+    }
+    }
+    
     public int getDia(){
         return this.dia;
     }
