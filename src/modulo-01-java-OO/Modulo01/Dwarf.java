cdd797fd27167c372b9fda57e5879a3286388d5d
Angelo <angelojunior50@yahoo.com.br>
2015-09-17 20:44:21 -0300

    Adição de dataNascimento

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -3,14 +3,21 @@ public class Dwarf{
     private Status status = Status.VIVO;
     private String nome;
     private int experiencia;
+    private DataTerceiraEra dataNascimento;
 
     public Dwarf (String nome){
+        this(nome, new DataTerceiraEra(1,1,1));
+    }
+
+    public Dwarf (String nome, DataTerceiraEra dataNascimento){
         this.nome = nome;
+        this.dataNascimento = dataNascimento;
     }
 
     public void receberFlechada(){
         this.vida -= 10;
         if(vida<=0){
+            vida = 0;
             status = Status.MORTO;
         }
     }
