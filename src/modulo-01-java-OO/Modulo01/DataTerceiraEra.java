7b4e9ea4e1fca3ffdd615243887739c3b9e127d3
Angelo Viana <angelojunior50@yahoo.com.br>
2015-09-17 17:32:38 -0300

    Exercicio 4

---
+++ src/modulo-01-java-OO/Modulo01/DataTerceiraEra.java
@@ -0,0 +1,17 @@
+public class DataTerceiraEra{
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public int getDia(){
+        return this.dia;
+    }
+
+    public int getMes(){
+        return this.mes;
+    }
+
+    public int getAno(){
+        return this.ano;
+    }
+}
