bfc76c679dd0f8cf85c1216c8779dadfce0bdbf6
Angelo <angelojunior50@yahoo.com.br>
2015-09-17 23:16:51 -0300

    Criando testes novos e arrumando antigos da classe Dwarf

--- src/modulo-01-java-OO/Modulo01/DwarfTest.java
+++ src/modulo-01-java-OO/Modulo01/DwarfTest.java
@@ -14,27 +14,76 @@ public class DwarfTest
 {
     @Test
     public void dwarfNasceCom110DeVida(){
-        Dwarf gimli = new Dwarf("jose");
-        assertEquals(110, gimli.getVida());
+        Dwarf dwarf = new Dwarf("jose");
+        assertEquals(110, dwarf.getVida());
     }
 
     @Test
-    public void dwarfPerdeVidaComFlechada(){
-        Dwarf gimli = new Dwarf("juliana");
-        int vidaOriginal = gimli.getVida();
-        gimli.receberFlechada();
-        assertNotSame(vidaOriginal, gimli.getVida());
-        assertTrue(gimli.getVida() < vidaOriginal);
+    public void dwarfSemDataEh111(){
+        Dwarf dwarf = new Dwarf("jose");
+        assertEquals(1, dwarf.getDataNascimento().getDia());
+        assertEquals(1, dwarf.getDataNascimento().getMes());
+        assertEquals(1, dwarf.getDataNascimento().getAno());
     }
-    
+
+    @Test
+    public void dwarfComDataEhCorreta(){
+        DataTerceiraEra data = new DataTerceiraEra(10,5,2008);
+        Dwarf dwarf = new Dwarf("jose", data);
+        assertEquals(10, dwarf.getDataNascimento().getDia());
+        assertEquals(5, dwarf.getDataNascimento().getMes());
+        assertEquals(2008, dwarf.getDataNascimento().getAno());
+    }
+
+    @Test
+    public void getNumeroSorteSeForBissextoEVidaEntre80E90(){
+        DataTerceiraEra data = new DataTerceiraEra(10,5,2008);
+        Dwarf dwarf = new Dwarf("jose", data);
+        dwarf.receberFlechada();
+        dwarf.receberFlechada();
+        assertEquals(-3333 , dwarf.getNumeroSorte(), 0.001);
+    }
+
+    @Test
+    public void naoEhBissextoMasEhSeixasOuMeireles(){
+        DataTerceiraEra data = new DataTerceiraEra(10,5,2007);
+        Dwarf dwarf1 = new Dwarf("Seixas", data);
+        Dwarf dwarf2 = new Dwarf("Meireles", data);
+        assertEquals(33 , dwarf1.getNumeroSorte() , 0.001);
+    }
+
+    @Test
+    public void dwarfAzaradoEComumPerdeVidaComFlechada(){
+        Dwarf dwarf = new Dwarf("Macacoloko");
+        dwarf.receberFlechada();
+        assertEquals(100 , dwarf.getVida());
+    }
     
     @Test
-    public void dwarfMorreCom0DeVida(){
-     Dwarf dwarf = new Dwarf("reginaldo");
-     for(int i=0;i<11;i++){
-     dwarf.receberFlechada();
+    public void dwarfEntre0E100NaoMudaNada(){
+        DataTerceiraEra data = new DataTerceiraEra(10,5,2007);
+        Dwarf dwarf = new Dwarf("Seixas", data);
+        dwarf.receberFlechada();
+        assertEquals(110, dwarf.getVida());
+        assertEquals(0, dwarf.getExperiencia());
     }
-    assertEquals(Status.MORTO, dwarf.getStatus());
+    @Test
+    public void dwarfSortudoGanhaXP(){
+        DataTerceiraEra data = new DataTerceiraEra(10,5,2008);
+        Dwarf dwarf = new Dwarf("jose", data);
+        dwarf.receberFlechada();
+        dwarf.receberFlechada();
+        dwarf.receberFlechada();
+        assertEquals(2, dwarf.getExperiencia());
+    }
+
+    @Test
+    public void dwarfMorreCom0DeVida(){
+        Dwarf dwarf = new Dwarf("reginaldo");
+        for(int i=0;i<11;i++){
+            dwarf.receberFlechada();
+        }
+        assertEquals(Status.MORTO, dwarf.getStatus());
     }
 
 }
