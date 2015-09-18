844fc5608f3be852fd23b9eca00b74ddfa5053fc
Angelo Viana <angelojunior50@yahoo.com.br>
2015-09-18 17:28:24 -0300

    adicionando classe Item

---
+++ src/modulo-01-java-OO/Modulo01/Item.java
@@ -0,0 +1,8 @@
+public class Item{
+    String descricao;
+    int quantidade;
+    public Item(String descricao, int quantidade){
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+    }
+}
