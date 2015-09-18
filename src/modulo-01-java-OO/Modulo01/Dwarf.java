2f8855da45c316b66bacca23f0195d94ef61d8aa
Angelo <angelojunior50@yahoo.com.br>
2015-09-17 21:31:03 -0300

    Adicionado numero da sorte

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -15,11 +15,25 @@ public class Dwarf{
     }
 
     public void receberFlechada(){
-        this.vida -= 10;
+        double numeroDaSorte = getNumeroSorte();
+        if(numeroDaSorte < 0){
+            experiencia++;
+        }else if(numeroDaSorte > 100){
+            this.vida -= 10;
+        }
         if(vida<=0){
             vida = 0;
             status = Status.MORTO;
         }
+    }
+
+    public double getNumeroSorte(){
+        if(this.dataNascimento.ehBissexto() && this.vida >= 80 && this.vida <= 90){
+            return 101 * -33;
+        }else if (!this.dataNascimento.ehBissexto() && this.nome.equals("Seixas") || this.nome.equals("Meireles")){
+            return (101 * 33) % 100;
+        }else
+            return 101;
     }
 
     public int getVida(){
