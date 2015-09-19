496f2ce0e7956f9e2916387e7ccf8595fffeb2ea
Angelo <angelojunior50@yahoo.com.br>
2015-09-18 22:39:43 -0300

    feat(Item):getters e equals

--- src/modulo-01-java-OO/Modulo01/Item.java
+++ src/modulo-01-java-OO/Modulo01/Item.java
@@ -1,8 +1,21 @@
 public class Item{
     String descricao;
     int quantidade;
-    public Item(String descricao, int quantidade){
+    public Item(int quantidade, String descricao){
         this.descricao = descricao;
         this.quantidade = quantidade;
+    }
+    
+    public String getDescricao(){
+        return this.descricao;
+    }
+    
+    public int getQuantidade(){
+        return this.quantidade;
+    }
+    
+    public boolean equals(Object obj){
+        Item item = (Item)obj;
+        return this.descricao.equals(item.getDescricao()) && this.quantidade == item.getQuantidade();
     }
 }
