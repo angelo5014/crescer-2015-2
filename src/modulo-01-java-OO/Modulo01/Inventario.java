4f53bb78a5670f95b24c1a32a177b4694eaa22c0
Angelo <angelojunior50@yahoo.com.br>
2015-09-20 21:05:12 -0300

    feat(Inventario) Adicionando e testando ordenarItem

--- src/modulo-01-java-OO/Modulo01/Inventario.java
+++ src/modulo-01-java-OO/Modulo01/Inventario.java
@@ -1,38 +1,50 @@
 import java.util.ArrayList;
 public class Inventario{
     private ArrayList<Item> itens = new ArrayList<>();
-    
+
     public void adicionarItem(Item item){
         itens.add(item);
     }
-    
+
     public void perderItem(Item item){
         itens.remove(item);
     }
-    
+
     public ArrayList<Item> getItens(){
         return this.itens;
     }
-    
+
     public String getDescricoesItens(){
         String retorno = "";
         int cont = 1;
-            for(Item item : this.itens){
-                retorno += item.getDescricao() + (cont != this.itens.size() ? "," : "");
-                cont++;
-            }
-            return retorno;
+        for(Item item : this.itens){
+            retorno += item.getDescricao() + (cont != this.itens.size() ? "," : "");
+            cont++;
+        }
+        return retorno;
     }
-    
+
     public Item getItemComMaiorQuantidade(){
         int maior = 0;
-        Item preferido = new Item(1, "teste");
+        Item preferido = new Item("teste", 1);
         for(Item item : itens){
             if(item.getQuantidade()>maior){
                 maior = item.getQuantidade();
                 preferido = item;
             }
-       }
-       return preferido;
+        }
+        return preferido;
+    }
+
+    public void ordenarItens(){    
+        for(int i = 0; i < itens.size(); i++) {
+            for(int j = 0; j < (itens.size()-1) ; j++) {
+                if(itens.get(j).getQuantidade() > itens.get(j+1).getQuantidade()) {
+                    Item temp = itens.get(j);
+                    itens.set(j, itens.get(j + 1));
+                    itens.set(j + 1, temp);
+                }
+            }
+        }
     }
 }
--- src/modulo-01-java-OO/Modulo01/InventarioTest.java
+++ src/modulo-01-java-OO/Modulo01/InventarioTest.java
@@ -9,14 +9,14 @@ public class InventarioTest
     @Test
     public void adicionarUmItem(){
         Dwarf dwarf = new Dwarf("reginaldo");
-        Item item = new Item(1 , "Espada");
+        Item item = new Item("Espada", 1);
         dwarf.getInventario().adicionarItem(item);
         assertEquals(dwarf.getInventario().getItens().get(0).getDescricao(), "Espada");
     }
 
     @Test
     public void adicionaItemFunciona(){
-        Item item = new Item(1 , "Espada");
+        Item item = new Item("Espada", 1);
         Dwarf dwarf = new Dwarf("reginaldo");
 
         dwarf.getInventario().adicionarItem(item);
@@ -26,8 +26,8 @@ public class InventarioTest
 
     @Test
     public void perdeItemFunciona(){
-        Item item = new Item(1 , "Espada");
-        Item item2 = new Item(1 , "Escudo");
+        Item item = new Item("Espada",1);
+        Item item2 = new Item("Escudo",1);
         Dwarf dwarf = new Dwarf("reginaldo");
 
         dwarf.getInventario().adicionarItem(item);
@@ -39,9 +39,9 @@ public class InventarioTest
 
     @Test
     public void imprimeDescricaoCorretamente(){
-        Item item = new Item(1 , "Espada");
-        Item item2 = new Item(1 , "Escudo");
-        Item item3 = new Item(1 , "Jabulani");
+        Item item = new Item("Espada",1);
+        Item item2 = new Item("Escudo",1);
+        Item item3 = new Item("Jabulani",1);
         Dwarf dwarf = new Dwarf("reginaldo");
 
         dwarf.getInventario().adicionarItem(item);
@@ -50,4 +50,52 @@ public class InventarioTest
 
         assertEquals("Espada,Escudo,Jabulani", dwarf.getInventario().getDescricoesItens());      
     }
+
+    @Test
+    public void ordenarInventarioCom5Itens(){
+        Dwarf dwarf = new Dwarf("teste");
+        Inventario inv = dwarf.getInventario();
+        Item item1 = new Item("a",5);
+        Item item2 = new Item("b",1);
+        inv.adicionarItem(item1);
+        inv.adicionarItem(item2);
+        dwarf.getInventario().ordenarItens();
+        assertEquals(1, inv.getItens().get(0).getQuantidade());
+
+    }
+
+    @Test 
+    public void itensOrdenadosDeFormaAscendente1(){
+        Inventario invDoThorin = new Inventario();
+        invDoThorin.adicionarItem(new Item("Pedra Arken",3));
+        invDoThorin.adicionarItem(new Item("Tererê de barba",5));
+        invDoThorin.adicionarItem(new Item("Espada emprestada do brother Légolas",1));
+        invDoThorin.ordenarItens();
+        assertEquals(1, invDoThorin.getItens().get(0).getQuantidade());
+
+    }
+
+    @Test 
+    public void itensOrdenadosDeFormaAscendente2(){
+        Inventario invDoThorin = new Inventario();
+        invDoThorin.adicionarItem(new Item("Pedra Arken", 3));
+        invDoThorin.adicionarItem(new Item("Tererê de barba", 5));
+        invDoThorin.adicionarItem(new Item("Espada emprestada do brother Légolas", 1));
+        invDoThorin.ordenarItens();
+
+        assertEquals(3, invDoThorin.getItens().get(1).getQuantidade());
+
+    }
+
+    @Test 
+    public void itensOrdenadosDeFormaAscendente3(){
+        Inventario invDoThorin = new Inventario();
+        invDoThorin.adicionarItem(new Item("Pedra Arken", 3));
+        invDoThorin.adicionarItem(new Item("Tererê de barba", 5));
+        invDoThorin.adicionarItem(new Item("Espada emprestada do brother Légolas", 1));
+        invDoThorin.ordenarItens();
+
+        assertEquals(5, invDoThorin.getItens().get(2).getQuantidade());
+    }
+
 }
