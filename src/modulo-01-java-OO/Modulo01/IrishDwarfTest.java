ed04ac8c252cc4e2fa8c82775fd8567ecc3903ca
Angelo Viana <angelojunior50@yahoo.com.br>
2015-09-21 17:38:12 -0300

    refactor(IrishDwarf): usando herança

--- src/modulo-01-java-OO/Modulo01/IrishDwarfTest.java
+++ src/modulo-01-java-OO/Modulo01/IrishDwarfTest.java
@@ -19,27 +19,20 @@ public class IrishDwarfTest
     }
 
     @Test
-    public void irishDwrafTem10EFicaCom90010(){
-        DataTerceiraEra data = new DataTerceiraEra(1,1,2000);
-        IrishDwarf dwarf = new IrishDwarf("ose", data);
+    public void irishDwarfComSorte() {
+        IrishDwarf dwarf = new IrishDwarf("Leprechaun sortudo", new DataTerceiraEra(1, 1, 2000));
         dwarf.receberFlechada();
         dwarf.receberFlechada();
-        Item item = new Item("batata", 10);
-        dwarf.getInventario().adicionarItem(item);
-        dwarf.tentarSorte();
-        assertEquals(90010, dwarf.getInventario().getItens().get(0).getQuantidade());
-    }
+        dwarf.getInventario().adicionarItem(new Item("Lança", 5));
+        dwarf.getInventario().adicionarItem(new Item("Poção",25));
 
-    @Test
-    public void irishDwrafTem1000EFicaCom90010(){
-        DataTerceiraEra data = new DataTerceiraEra(1,1,2000);
-        IrishDwarf dwarf = new IrishDwarf("ose", data);
-        dwarf.receberFlechada();
-        dwarf.receberFlechada();
-        Item item = new Item("batata", 1000);
-        dwarf.getInventario().adicionarItem(item);
+        Inventario esperado = new Inventario();
+        esperado.adicionarItem(new Item("Lança", 15005));
+        esperado.adicionarItem(new Item("Poção", 325025));
+
         dwarf.tentarSorte();
-        assertEquals(999001000, dwarf.getInventario().getItens().get(0).getQuantidade());
+
+        assertEquals(esperado, dwarf.getInventario());
     }
 
     @Test public void irishDwrafTem3EContinuaCom3(){
