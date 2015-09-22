4d98de8cc91dc6b1c2d2f6984750999213a9f451
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-09-22 11:50:07 -0300

    completando classe abstrata orc

--- src/modulo-01-java-OO/Modulo01/Orc.java
+++ src/modulo-01-java-OO/Modulo01/Orc.java
@@ -1,12 +1,36 @@
-public class Orc{
-    int vida;
-    Status status;
+abstract class Orc{
+    protected int vida;
+    protected Status status;
+    protected Inventario inventario;
     
-    public void atacarInimigo(Elfo elfo){
-        
+    public Orc(){
+        inventario = new Inventario();
     }
     
-    public void atacarInimigo(Dwarf dwarf){
+    public void serAtacado(Object obj){
+        if(obj instanceof Elfo){
+            this.vida -= 8;
+        }else if(obj instanceof Dwarf){
+            if(inventario.procurarItemPelaDescricaoBoolean("Escudo Uruk-Hai")){
+                this.vida -= 5;
+            }else{
+                this.vida -= 10;
+            }
+        }
+        if(this.vida<=0){
+            this.status = Status.MORTO;
+            this.vida = 0;
+        }
     }
     
+    public void atacarInimigo(Object obj){
+    }
+    
+    public int getVida(){
+        return this.vida;
+    }
+    
+    public Inventario getInventario(){
+        return this.inventario;
+    }
 }
