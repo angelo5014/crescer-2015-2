362ba2a480acbfb67091b76dd0ee08994ef16482
Angelo Viana <angelojunior50@yahoo.com.br>
2015-09-22 15:38:33 -0300

    add(Personagem):adicionado classe abstrada personagem

---
+++ src/modulo-01-java-OO/Modulo01/Personagem.java
@@ -0,0 +1,42 @@
+public abstract class Personagem{
+    protected String nome;
+    protected int vida;
+    protected int experiencia;
+    protected Status status;
+    protected Inventario inventario;
+    
+    public Personagem(){
+        this(null);
+    }
+    
+    public Personagem(String nome){
+        this.nome = nome;
+    }
+    
+    public Inventario getInventario(){
+        return this.inventario;
+    }
+
+    public int getVida(){
+        return this.vida;
+    }
+
+    public Status getStatus(){
+        return this.status; 
+    }
+
+    public int getExperiencia(){
+        return this.experiencia;
+    }  
+    
+    public void adicionarItem(Item item){
+        inventario.getItens().add(item);
+    }
+
+    public void perderItem(Item item){
+        inventario.getItens().remove(item);
+    }
+    
+    public void serAtacado(Object obj){
+    }
+}
