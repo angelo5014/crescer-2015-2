882a8d4f21630d56858c287395718e9c19af24cc
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-09-22 11:49:20 -0300

    melhorando metodo aturar flecha

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -2,6 +2,7 @@
  * Representa objetos do tipo Elfo.
  */
 public class Elfo {
+    private int vida = 80;
     private String nome;
     private int flechas;
     private int experiencia = 0;
@@ -17,37 +18,50 @@ public class Elfo {
         this.status = Status.VIVO;
     }
 
-    public void atirarFlechas(Dwarf alvo){
-        flechas--;
-        alvo.receberFlechada();
-        experiencia++;
+    public void atirarFlechas(Object obj){
+        if(obj instanceof Dwarf){
+            Dwarf dwarf = (Dwarf)obj;
+            flechas--;
+            dwarf.receberFlechada();
+            experiencia++;
+        }else if(obj instanceof UrukHai){
+            UrukHai urukHai = (UrukHai)obj;
+            urukHai.serAtacado(this);
+        }else if(obj instanceof Snaga){
+            Snaga snaga = (Snaga)obj;
+            snaga.serAtacado(this);
+        }
     }
 
     public String toString(){
         boolean flechaNoSingular = this.flechas != 1;
         boolean nivelNoSingular = this.experiencia != 1;
-        /*
-        if(this.flechas != 1){
-            textoFlechas += "s";
-        }
-        if(this.experiencia != 1){
-         textoNiveis = "níveis";   
-        }
-        */
-        
+
         String textoFormatado = String.format("%s possui %d %s e %d %s de experiencia", 
-        this.nome,
-        this.flechas,
-        flechaNoSingular ? "flechas" : "flecha",
-        this.experiencia,
-        nivelNoSingular ? "níveis" : "nível"
-        );
-        
+                this.nome,
+                this.flechas,
+                flechaNoSingular ? "flechas" : "flecha",
+                this.experiencia,
+                nivelNoSingular ? "níveis" : "nível"
+            );
+
         return textoFormatado;//this.nome + " possui " + this.flechas + " " + textoFlechas + " e " + this.experiencia + " " +textoNiveis + " de experiencia";
+    }
+
+    public void serAtacado(Object obj){
+        if(obj instanceof UrukHai){
+            this.vida -= 12;
+        }else if(obj instanceof Snaga){
+            this.vida -= 8;
+        }
     }
 
     public String getNome(){
         return this.nome;
+    }
+    
+    public int getVida(){
+        return this.vida;
     }
 
     public int getFlechas(){
@@ -57,7 +71,7 @@ public class Elfo {
     public int getExperiencia(){
         return this.experiencia;
     }
-    
+
     public Status getStatus(){
         return this.status;
     }
