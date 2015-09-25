8d57c4281599329c8df7902457eabccef1ecd03e
Angelo Viana <angelojunior50@yahoo.com.br>
2015-09-25 16:33:44 -0300

    feat(EstrategiaNormal): adicionado estrategia normal e testada

---
+++ src/modulo-01-java-OO/Modulo01/EstrategiaNormal.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class EstrategiaNormal implements EstrategiaDeAtaque{
+    private ArrayList<Elfo> ordemDoUltimoAtaque = new ArrayList<Elfo>();
+    
+    public void atacar(HashMap<Status, ArrayList<Elfo>> exercito, ArrayList<Dwarf> dwarves){
+        ArrayList<Elfo> pelotao = exercito.get(Status.VIVO);
+        int intencoes = pelotao.size() * dwarves.size();
+        int cont = 0;
+
+        for(Elfo elfo : pelotao){
+            this.ordemDoUltimoAtaque.add(elfo);
+            if(elfo instanceof ElfoNoturno){
+                if(cont < 0.3 * intencoes){
+                    cont++;
+                    for(Dwarf dwarf: dwarves){
+                        elfo.atacar(dwarf);
+                    }
+                }
+            }else{
+                for(Dwarf dwarf: dwarves){
+                    elfo.atacar(dwarf);
+                }
+            }
+        }
+    }
+    
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return this.ordemDoUltimoAtaque;
+    }
+}
