2560bb1573d56af6b7578b5947f158b99fce3b68
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-10-20 23:27:17 -0200

    exercicios dia 3

--- src/modulo-03-web/dia-03/carrinhoDeCompras.js
+++ src/modulo-03-web/dia-03/carrinhoDeCompras.js
@@ -1,11 +1,27 @@
-function carrinhoDeCompras(){
-  produtos = [];
+function CarrinhoDeCompras(){
+  this.produtos = [];
 }
 
-carrinhoDeCompras.prototype.adicionarItem = function(item){
-  produtos.push(item);
+CarrinhoDeCompras.prototype.adicionarItem = function(item){
+  this.produtos.push(item);
 }
 
-carrinhoDeCompras.prototype.removerItem = function(sku){
-    produtos.indexOf(produtos.sku);
-  }
+CarrinhoDeCompras.prototype.removerItem = function(sku){
+    this.produtos.slice(indexOfProduto(sku),1);
+}
+
+CarrinhoDeCompras.prototype.atualizarItem = function(sku, novaQtd){
+  this.produtos[indexOfProduto(sku)].quantidade = novaQtd;
+}
+
+CarrinhoDeCompras.prototype.calcularValorTotal = function(){
+  var soma = 0;
+  this.produtos.forEach(function(a){soma += a.calcularSubTotal(a);});
+  return soma;
+}
+
+CarrinhoDeCompras.prototype.indexOfProduto = function(sku){
+ this.produtos.map(function(a){
+   return a.sku;
+ }).indexOf(sku);
+}
--- src/modulo-03-web/dia-03/item.js
+++ src/modulo-03-web/dia-03/item.js
@@ -1,6 +1,9 @@
-function item(sku, descricao, quantidade, valorUnitario){
+function Item(sku, descricao, quantidade, valorUnitario){
   this.sku = sku;
   this.descricao = descricao;
   this.quantidade = quantidade;
   this.valorUnitario = valorUnitario;
+}
+Item.prototype.calcularSubTotal = function(item){
+  return item.quantidade * item.valorUnitario;
 }
