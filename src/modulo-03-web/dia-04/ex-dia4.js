6a597a880cfbda135be5c631fd49b53daf6eea9d
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-10-21 21:02:00 -0200

    feat(ex-dia4.js): adicionar funçao isBissext

--- src/modulo-03-web/dia-04/ex-dia4.js
+++ src/modulo-03-web/dia-04/ex-dia4.js
@@ -2,3 +2,8 @@ function isPalindrome(palavra){
   var palavraInvertida = palavra.split('').reverse().join('');
   return palavra === palavraInvertida ? true : false;
 }
+
+function isBissext(data){
+  var ano = data.getFullYear();
+  return ((ano % 4 === 0) && (ano % 100 !== 0) || (ano %400 === 0));
+}
