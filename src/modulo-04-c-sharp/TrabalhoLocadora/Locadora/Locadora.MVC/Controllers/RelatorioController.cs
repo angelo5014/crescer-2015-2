45380fc62e07cae6c46931fd4a682f5f0a8b0be7
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-11-03 21:51:39 -0200

    versionando MVC Locadora

---
+++ src/modulo-04-c-sharp/TrabalhoLocadora/Locadora/Locadora.MVC/Controllers/RelatorioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora.MVC.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        public ActionResult JogosDisponiveis()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/modulo-04-c-sharp/TrabalhoLocadora/Locadora/Locadora.MVC/Models/RelatorioModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.MVC.Models
+{
+    public class RelatorioModel
+    {
+    }
+}
