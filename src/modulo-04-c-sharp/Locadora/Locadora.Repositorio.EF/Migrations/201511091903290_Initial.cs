cf676f058af4a14821d3456345268a9f583f5c94
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-11-10 13:59:48 -0200

    Erros no migration e populando tabela

--- src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Migrations/201511091903290_Initial.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Migrations/201511091903290_Initial.cs
@@ -20,19 +20,23 @@ namespace Locadora.Repositorio.EF.Migrations
                 "dbo.Categoria",
                 c => new
                 {
-                    Id = c.Int(nullable: false, identity: true),
+                    Id = c.Int(nullable: false, identity: false),
                     Descricao = c.String(nullable: false)
                 })
                 .PrimaryKey(t => t.Id);
+
+            CriarCategorias();
 
             CreateTable(
                 "dbo.Selo",
                 c => new
                 {
-                    Id = c.Int(nullable: false, identity: true),
+                    Id = c.Int(nullable: false, identity: false),
                     Descricao = c.String(nullable: false)
                 })
                 .PrimaryKey(t => t.Id);
+
+            CriarSelos();
 
             CreateTable(
                 "dbo.Jogo",
@@ -50,18 +54,40 @@ namespace Locadora.Repositorio.EF.Migrations
                 })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Cliente", t => t.IdClienteLocacao)
-                .ForeignKey("dbo.IdCategoria", t => t.IdCategoria)
-                .ForeignKey("dbo.IdSelo", t => t.IdSelo)
-                .Index(t => t.IdClienteLocacao);
+                .ForeignKey("dbo.Categoria", t => t.IdCategoria)
+                .ForeignKey("dbo.Selo", t => t.IdSelo)
+                .Index(t => t.IdClienteLocacao)
+                .Index(t => t.IdSelo)
+                .Index(t => t.IdCategoria);
 
         }
-        
+
+        public void CriarSelos()
+        {
+            Sql("INSERT INTO Selo VALUES(1, 'Ouro')");
+            Sql("INSERT INTO Selo VALUES(2, 'Prata')");
+            Sql("INSERT INTO Selo VALUES(3, 'Bronze')");
+        }
+
+        public void CriarCategorias()
+        {
+            Sql("INSERT INTO Categoria(Id, Descricao) VALUES(1, 'RPG')");
+            Sql("INSERT INTO Categoria(Id, Descricao) VALUES(2, 'Corrida')");
+            Sql("INSERT INTO Categoria(Id, Descricao) VALUES(3, 'Aventura')");
+            Sql("INSERT INTO Categoria(Id, Descricao) VALUES(4, 'Luta')");
+            Sql("INSERT INTO Categoria(Id, Descricao) VALUES(5, 'Esporte')");
+        }
+
         public override void Down()
         {
             DropForeignKey("dbo.Jogo", "IdClienteLocacao", "dbo.Cliente");
-            DropIndex("dbo.Jogo", new[] { "IdClienteLocacao" });
+            DropForeignKey("dbo.Jogo", "IdCategoria", "dbo.Categoria");
+            DropForeignKey("dbo.Jogo", "IdSelo", "dbo.Selo");
+            DropIndex("dbo.Jogo", new[] { "IdClienteLocacao", "IdSelo", "IdCategoria" });
             DropTable("dbo.Jogo");
             DropTable("dbo.Cliente");
+            DropTable("dbo.Selo");
+            DropTable("dbo.Categoria");
         }
     }
 }
