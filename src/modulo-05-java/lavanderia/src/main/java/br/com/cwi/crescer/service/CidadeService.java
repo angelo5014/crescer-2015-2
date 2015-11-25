eb72719022e7d88f0c017c4a785bcb157aebdc6b
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-11-24 22:08:14 -0200

    correçao de bugs

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/service/CidadeService.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/service/CidadeService.java
@@ -2,6 +2,7 @@ package br.com.cwi.crescer.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.cwi.crescer.dao.CidadeDAO;
@@ -12,6 +13,7 @@ public class CidadeService {
 
 	private CidadeDAO cidadeDAO;
 
+	@Autowired
 	public CidadeService(CidadeDAO cidadeDAO){
 		this.cidadeDAO = cidadeDAO;
 	}
